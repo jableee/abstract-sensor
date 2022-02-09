ca88ad9e30f88fb632ed9ff9d4d695d43e23f57e
jableee <96569903+jableee@users.noreply.github.com>
2022-02-10 01:12:07 +0900

    심화 index.js
    심화 index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
-class Sensor {}
+class Sensor {
+    constructor(getid) {
+        this.id = getid;
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+    }
 
-class IotServer {}
+    turn(onoff) {
+        if (this.powerStatus === 'on' && onoff === 'on') throw new Error();
+        this.powerStatus = onoff;
+        this.status = 'idle';
+
+        setTimeout(() => {
+            this.status = 'sensingDistance';
+        }, this.reportingInterval);
+        const under500 = Math.random() * 500;
+        setTimeout(() => {
+            this.status = 'reportingData';
+        }, this.reportingInterval + Math.floor(under500));
+        // console.log(this.reportingInterval + Math.floor(under500), 'addnum' + Math.floor(under500));
+        const under1000 = Math.random() * 1000;
+        setTimeout(() => {
+            this.status = 'idle';
+        }, this.reportingInterval + Math.floor(under500) + Math.floor(under1000));
+        // console.log(
+        //     this.reportingInterval + Math.floor(under500) + Math.floor(under1000),
+        //     'addnum' + Math.floor(under1000),
+        // );
+    }
+}
+
+class IotServer {
+    constructor() {
+        this.getsensor = [];
+    }
+
+    start(sensor) {
+        this.getsensor.push(...sensor);
+    }
+
+    publish({ deviceId, actionId, payload }) {
+        for (const sensor of this.getsensor) {
+            if (sensor.id === deviceId && sensor.powerStatus === 'on' && actionId === 'CHANGE_REPORTING_INTERVAL') {
+                return (sensor.reportingInterval = payload);
+            }
+        }
+    }
+}
 
 module.exports = {
     Sensor,
